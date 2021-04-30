52c76f511ba9bb63fdee8b5e05a6cd93ab2aff2e
chava5 <djlmod5@gmail.com>
2021-04-30 11:41:41 -0500

    Implementación de conexión a la base de datos: Capa Infrastructure

---
+++ APIPersonal.Infrastructure/Connection/APIPERSONALContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIPersonal.Infrastructure.Connection
+{
+    class APIPERSONALContext
+    {
+    }
+}
---
+++ APIPersonal.Infrastructure/DataAccess/CPersona.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIPersonal.Infrastructure.DataAccess
+{
+    class CPersona
+    {
+    }
+}
